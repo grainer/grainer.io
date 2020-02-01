4223d639b65cfda5881540a672697bd2c8cb122c
gimyboya <b.afatah@yahoo.com>
2020-02-02 02:04:00 +0800

    fix(analytics): used a more compressed syntax

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,12 +27,10 @@ module.exports = {
     script: [
       { src: 'https://www.googletagmanager.com/gtag/js?id=G-79XRKWDMBD', async: true },
       {
-        innerHTML: `window.dataLayer = window.dataLayer || []
-                    function gtag() {
-                      dataLayer.push(arguments)
-                    }
-                    gtag('js', new Date())
-                    gtag('config', 'G-79XRKWDMBD')
+        innerHTML: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-79XRKWDMBD');
                     `
       }
     ]
