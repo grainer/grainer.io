2b0008cf797ae5147168d300e450a11b2950d354
imamkharia <imamkhaira@gmail.com>
2020-06-23 05:59:17 +0700

    feat(home): initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2020
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
-  // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'space-before-function-paren': ['error', 'never'],
-    'vue/html-self-closing': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/max-attributes-per-line': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
--- middleware/beforEach.js
+++
@@ -1,5 +0,0 @@
-import { EventBus } from '@/plugins/eventBus.js'
-
-export default function(context) {
-  EventBus.$emit('reveal', 'top')
-}
